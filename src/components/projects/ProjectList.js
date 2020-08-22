15bd438189b3ed12f87f64a8017a211054ed76fb
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-08-22 13:57:40 +0530

    edits-1:57pm

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ProjectSummary from './ProjectSummary';
 
 const ProjectList = ({projects}) => {
-    const projList = projects.length ? (
+    const projList = projects ? (
         projects.map(project => {
             return(
                 <ProjectSummary project={project} key={project.id} />
